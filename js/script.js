94d8e025f7197a59366f1ca0a18ce733875b5436
jessdobs <jess.dobs@gmail.com>
2024-04-08 11:17:11 -0400

    restart employee directory project after break includes javascript to create html elements

--- js/script.js
+++ js/script.js
@@ -1,183 +1,93 @@
-// Global Variables
-let employees;
-let currentEmployee;
-let gallery = document.querySelector('#gallery');
-let modalContainer; //modal container
-let modalInfo; //employee specific info inside modal
+/* Global Variables */
+const search = createSearchForm();
+const gallery = document.getElementById('gallery');
+const modal = createModalSkeleton();
+const modalClose = document.querySelector('.modal-close-btn');
 
-// Fetch Random Users from the Random Users API
-fetch('https://randomuser.me/api/?results=12&exc=gender,login,registered,phone,id&nat=US')
-    .then(result => result.json())
-    .then(data => cacheEmployees(data))
-    .then(employees => displayGallery(employees))
-    .catch(err => console.log(new Error(err)));
+/* Display Functions */
+function createSearchForm() {
+    const searchContainer = document.querySelector('.search-container');
+    const html = `
+        <form action='#' method='get'>
+            <input type='search' id='search-input' class='search-input' placeholder='Search...'>
+            <input type='submit' value='&#x1F50D;' id='search-submit class='search-submit'>
+        </form>
+    `;
 
-// Function calls to set initial page view
-createModalSkeleton();
-
-// Helper Functions
-/** Stores employees returned from fetch
- * @param {object} data json of fetch results
- * @returns @type {array} of employees
- */
-function cacheEmployees(data) {
-    employees = data.results.map(element => element);
-    return employees;
-}
-
-/** Creates employee cards for each employee in the employees array and appends the html to the gallery
- * @param {array} employees 
- */
-function displayGallery(array) {
-    array.forEach(employee => {
-        let cardHtml = `
-            <div id="${employee.email}" class="card">
-                <div class="card-img-container">
-                    <img class="card-img" src="${employee.picture.large}" alt="Profile Picture of ${employee.name.first} ${employee.name.last}">
-                </div>
-                <div class="card-info-container">
-                    <h3 id="name" class="card-name cap">${employee.name.first} ${employee.name.last}</h3>
-                    <p id="email" class="card-text">${employee.email}</p>
-                    <p id="city-state"class="card-text">${employee.location.city}, ${employee.location.state}</p>
-                </div>
-            </div>    
-        `;
-        gallery.insertAdjacentHTML('beforeend', cardHtml);
-    });
-}
-
-/** Resets innerHTML of gallery */
-function clearGallery() {
-    gallery.innerHTML = '';
+    searchContainer.insertAdjacentHTML('beforeend', html);
+    return document.querySelector('form');
 };
 
-/** Adds static html for the modal and sets the modalContainer variable */
 function createModalSkeleton() {
-    let modalHtml = `
-        <div class="modal-container hide">
-            <div class="modal">
-                <button type="button" id="modal-close-btn" class="modal-close-btn">X</button>
-                
-                <div class="modal-info-container"></div>
+    const modalHtml = `
+        <div class='modal-container hide'>
+            <div class='modal'>
+                <button type='button' id='modal-close-btn' class='modal-close-btn'>
+                    <strong>X</strong>
+                </button>
+
+                <div class='modal-info-container'>
+                </div>
             </div>
-            
-            <div class="modal-btn-container">
-                <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
-                <button type="button" id="modal-next" class="modal-next btn">Next</button>
+
+            <div class='modal-btn-container'>
+                <button type='button' id='modal-prev' class='modal-prev btn'>Prev</button>
+                <button type='button' id='modal-next' class='modal-next btn'>Next</button>
             </div>
         </div>
     `;
-    gallery.insertAdjacentHTML('afterend', modalHtml);
-    modalContainer = gallery.nextElementSibling;
-}
 
-/** Toggles modal visiblity based on provided param
- * @param {string} action 
- */
-function toggleModalVisibility(action) {
-    if (action ==='show') {
-        modalContainer.classList.remove('hide');
-    } else if (action === 'hide') {
-        modalContainer.classList.add('hide');
-    }
-}
-
-/** Sthe currentEmployee variable using the supplied email as the unique identifier then calls @function showEmployeeDetails to populate the modal's dynamic content
- * @param {string} email 
- */
-function setCurrentEmployee(empId) {
-    currentEmployee = employees.find(employee => employee.email === empId);
-    showEmployeeDetails(currentEmployee);
+    document.body.insertAdjacentHTML(`beforeend`, modalHtml);
+    return document.querySelector('.modal-container');
 };
 
-/** Sets the dynamic content of the modal based on the supplied employee
- * @param {object} employee 
+function displayEmployeeGrid(employees) {
+    employees.forEach(employee => {
+        const html = `
+            <div class='card'>
+                <div class='card-img-container'>
+                    <img class='card-img' src='' alt=''>
+                </div>
+                <div class='card-info-container'>
+                    <h3 id='name' class='card-name' cap><h3>
+                    <p class='card-text'></p>
+                    <p class='card-text cap'></p>
+                </div>
+            </div>
+        `;
+
+        gallery.insertAdjacentHTML(`beforeend`, html);
+    });
+};
+/**
+ * Call function when user clicks on an employee card from the grid OR uses the modal navigation to view prev/next employee
+ * @param {*} employee 
  */
-function showEmployeeDetails(employee) {
-    modalInfo = modalContainer.firstElementChild.lastElementChild;
-    
-    let employeeDetailsHtml = `
-        <img class="modal-img" src="${employee.picture.large}" alt="Profile Picture of ${employee.name.first} ${employee.name.last}">
-        <h3 id="name" class="modal-name cap">${employee.name.first} ${employee.name.last}</h3>
-        <p class="modal-text">${employee.email}</p>
-        <p class="modal-text">${employee.location.city}</p>
-
+function displayEmployeeDetails(employee) {
+    let modalInfoContainer = document.querySelector('.modal-info-container');
+    const infoHtml = `
+        <img class='modal-img' src='' alt=''>
+        <h3 id='name' class='modal-name cap'></h3>
+        <p class='modal-text'></p>
+        <p class='modal-text'></p>
         <hr>
-
-        <p class="modal-text">${employee.cell}</p>
-        <p class="modal-text">${employee.location.street.number} ${employee.location.street.name}, ${employee.location.city}, ${employee.location.state} ${employee.location.postcode}</p>
-        <p class="modal-text">${employee.email}</p>
+        <p class='modal-text'></p>
+        <p class='modal-text'></p>
+        <p class='modal-text'></p>
     `;
 
-    modalInfo.insertAdjacentHTML('beforeend', employeeDetailsHtml);
-    toggleModalVisibility('show');
-}
-
-/** Removes dynamic content (employee details) from modal */
-function clearEmployeeDetails() {
-    modalInfo.innerHTML = '';
-}
-
-/** Gets the index of the currentEmployee and reduces the index by 1 or resets the index to the last element in the employees array and calls the @function setCurrentEmployee */
-function getPrevEmployee() {
-    let currentIndex = employees.indexOf(currentEmployee);
-    let employeeId;
-
-    if (currentIndex !== 0) {
-        currentIndex --;
-    } else {
-        currentIndex = employees.length - 1;
-    }
-
-    employeeId = employees[currentIndex].email;
-    setCurrentEmployee(employeeId);
+    modalInfoContainer.insertAdjacentHTML(`beforeend`, infoHtml);
+    
+    if (modal.classList.contains('hide')) {
+        modal.classList.remove('hide');
+    };
 };
 
-/** Gets the index of the currentEmployee and increases the index by 1 or resets the index to the first element in the employees array and calls the @function setCurrentEmployee */
-function getNextEmployee() {
-    let currentIndex = employees.indexOf(currentEmployee);
-    let employeeId;
-
-    if (currentIndex !== employees.length - 1) {
-        currentIndex ++;
-    } else {
-        currentIndex = 0;
-    }
-
-    employeeId = employees[currentIndex].email;
-    setCurrentEmployee(employeeId);
-};
-
-// Event listeners
-gallery.addEventListener('click', e => {
-    if (e.target.id !== 'gallery') {
-        const targetElement = e.target;
-        let employeeId;
-        
-        if (targetElement.className === 'card') {
-            employeeId = targetElement.id;
-        } else if (targetElement.tagName === 'DIV') {
-            employeeId = targetElement.parentElement.id;
-        } else if (targetElement.className === 'card-img' || targetElement.id === 'name' || targetElement.className === 'card-text') {
-            employeeId = targetElement.parentElement.parentElement.id;
-        }
-        setCurrentEmployee(employeeId);
-    }
+/* Event Handlers */
+modalClose.addEventListener('click', () => {
+    modal.classList.add('hide');
 });
 
-modalContainer.addEventListener('click', e => {
-    if (e.target.tagName === 'BUTTON') {
-        const button = e.target;
 
-        if (button.id === 'modal-close-btn') {
-            toggleModalVisibility('hide');
-            clearEmployeeDetails();
-        } else if (button.id === 'modal-prev') {
-            clearEmployeeDetails();
-            getPrevEmployee();
-        } else if (button.id === 'modal-next') {
-            clearEmployeeDetails();
-            getNextEmployee();
-        }
-    }
-});
+displayEmployeeDetails();
+
